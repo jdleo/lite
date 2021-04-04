2d1b685ca40424ed01466adae7172dd8d2624bc0
John Leonardo <j@jdleo.me>
2021-04-04 16:33:33 -0500

    logic for copying text to clipboard

--- src/components/LinkOutput.js
+++ src/components/LinkOutput.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -5,15 +6,57 @@ import useWindowDimensions from '../helpers/useWindowDimensions';
 import { faCopy } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function LinkOutput({ shortLink }) {
+export default function LinkOutput({ shortLink, setShortLink }) {
   // state mgmt
   const { _, width } = useWindowDimensions();
+  const [isCopying, setIsCopying] = useState(false);
+
+  // helper method to handle when container is tapped (should copy text)
+  const handleCopy = () => {
+    if (isCopying) return;
+
+    // create temporary text area
+    var textArea = document.createElement('textarea');
+    textArea.value = shortLink;
+
+    // avoid scrolling to bottom
+    textArea.style.top = '0';
+    textArea.style.left = '0';
+    textArea.style.position = 'fixed';
+
+    // focus and select
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    // try to copy
+    try {
+      var successful = document.execCommand('copy');
+    } catch (err) {}
+
+    // show copied to user, and get temp text
+    setIsCopying(true);
+    const temp = shortLink;
+    setShortLink('Copied!');
+
+    // remove temp text area
+    document.body.removeChild(textArea);
+
+    // after 1 second, remove copied text
+    setTimeout(() => {
+      setIsCopying(false);
+      setShortLink(temp);
+    }, 1000);
+  };
 
   return (
-    <Container style={{ ...styles.container, ...(width > 500 ? { width: '40%' } : { width: '80%' }) }}>
+    <Container
+      style={{ ...styles.container, ...(width > 500 ? { width: '40%' } : { width: '80%' }) }}
+      onClick={() => handleCopy()}
+    >
       <Row style={styles.row}>
         <Col xs={9} className="align-self-center text-center" style={styles.linkContainer}>
-          {shortLink}
+          <span>{shortLink}</span>
         </Col>
         <Col xs={3} className="align-self-center text-center" style={styles.copyContainer}>
           <FontAwesomeIcon icon={faCopy} />
@@ -31,6 +74,7 @@ const styles = {
     height: 70,
     borderRadius: 8,
     padding: 0,
+    cursor: 'pointer',
   },
   col: { textAlign: 'center', verticalAlign: 'middle' },
   linkContainer: { fontFamily: "'Open Sans', sans-serif", fontWeight: 600, fontSize: '1.4em' },
