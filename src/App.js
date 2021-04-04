ef3ff10740d0209f258d071718083ce0cafa8fb6
John Leonardo <j@jdleo.me>
2021-04-04 16:45:35 -0500

    pass setError into LinkInput

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import { useState } from 'react';
 import './App.css';
 
-import { Navbar, Title, LinkInput, LinkOutput, Wave } from './components';
+import { Navbar, Title, LinkInput, LinkOutput, Wave, Error } from './components';
 
 function App() {
   // state mgmt
   const [link, setLink] = useState('');
   const [shortLink, setShortLink] = useState('');
+  const [error, setError] = useState('');
 
   return (
     <>
       <Navbar />
       <Title />
-      <LinkInput link={link} onChange={e => setLink(e.target.value)} setShortLink={setShortLink} />
+      <LinkInput link={link} onChange={e => setLink(e.target.value)} setShortLink={setShortLink} setError={setError} />
+      {error !== '' && <Error error={error} />}
       {shortLink !== '' && <LinkOutput shortLink={shortLink} setShortLink={setShortLink} />}
       <Wave />
     </>
