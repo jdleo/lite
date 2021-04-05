67bbf75a5fd6937e023d3efb691326232ccbb5ce
John Leonardo <j@jdleo.me>
2021-04-04 22:48:33 -0500

    pass setLoading into LinkInput

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import { useState } from 'react';
 import './App.css';
 
-import { Navbar, Title, LinkInput, LinkOutput, Wave, Error } from './components';
+import { Navbar, Title, LinkInput, LinkOutput, Wave, Error, Spinner } from './components';
 
 function App() {
   // state mgmt
   const [link, setLink] = useState('');
   const [shortLink, setShortLink] = useState('');
   const [error, setError] = useState('');
+  const [loading, setLoading] = useState(true);
 
   return (
     <>
       <Navbar />
       <Title />
-      <LinkInput link={link} onChange={e => setLink(e.target.value)} setShortLink={setShortLink} setError={setError} />
+      <LinkInput
+        link={link}
+        onChange={e => setLink(e.target.value)}
+        setShortLink={setShortLink}
+        setError={setError}
+        setLoading={setLoading}
+      />
+      <Spinner loading={loading} />
       {error !== '' && <Error error={error} />}
       {shortLink !== '' && <LinkOutput shortLink={shortLink} setShortLink={setShortLink} />}
       <Wave />
