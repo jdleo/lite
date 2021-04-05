a5b570d5faa7cf932fc6e2bffbabee61b2560cf1
John Leonardo <j@jdleo.me>
2021-04-04 22:48:00 -0500

    style loader

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,1 +1,16 @@
-export default function Spinner() {}
+import PuffLoader from 'react-spinners/PuffLoader';
+
+export default function Spinner({ loading }) {
+  return (
+    <div style={styles.container}>
+      <PuffLoader color={'#ff9bf5'} size={100} loading={loading} css={{ display: 'block', margin: '0 auto' }} />
+    </div>
+  );
+}
+
+const styles = {
+  container: {
+    margin: 'auto',
+    marginTop: 50,
+  },
+};
