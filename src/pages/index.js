8aa21d004b6acfe876413d49072d6166b5bbbe32
John Leonardo <j@jdleo.me>
2025-12-26 14:19:49 -0800

    feat: Add Google site verification meta tag to homepage.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import LinkInput from '../components/LinkInput';
 import Navbar from '../components/Navbar';
 import Title from '../components/Title';
@@ -10,6 +11,9 @@ import Stats from '../components/Stats';
 export default function Home() {
     return (
         <>
+            <Head>
+                <meta name="google-site-verification" content="dhSzTdOJZ38TMDMcHz_0Wy1CY3i-Pja-AtUK66Po6MY" />
+            </Head>
             <Navbar />
             <Title />
             <Stats />
